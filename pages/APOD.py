5bd2cff0cf2704132732d2dade7eac2327c21f65
zwelshman <zmswelshman@gmail.com>
2024-07-25 17:52:41 +0100

    hdruk

--- pages/APOD.py
+++ pages/APOD.py
@@ -15,19 +15,14 @@ st.set_page_config(page_title="Astronomy Picture of the Day", layout="wide")
 st.title("Astronomy Picture of the Day")
 
 apod_data = fetch_apod(api_key)
+import pprint 
+
+st.write((apod_data))
 
 col1, col2 = st.columns([3, 2])
 
-import requests
-from PIL import Image
-from io import BytesIO
-
-response = requests.get(apod_data['url'])
-
 with col1:
-    # st.image(apod_data['url'], caption=apod_data['title'], use_column_width=True)
-    img = Image.open(BytesIO(response.content))
-    st.image(img, caption=apod_data['title'], use_column_width=True)
+    st.image(apod_data['hdurl'], caption=apod_data['title'], use_column_width=True)
 
 with col2:
     st.write(apod_data['explanation'])
