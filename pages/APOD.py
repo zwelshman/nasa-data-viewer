0f52dcf6f31eba0a78d70b02bcc058d82ad7a0b2
Zach Welshman <38403427+zwelshman@users.noreply.github.com>
2024-07-26 08:30:41 +0100

    Update APOD.py

--- pages/APOD.py
+++ pages/APOD.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 api_key = os.environ['NASA_API_KEY']
 
-@st.cache_data(ttl=86400)  # Cache for 24 hours
+@st.cache_data(ttl=43200)  # Cache for 24 hours
 def fetch_apod(api_key):
     url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
     response = requests.get(url)
